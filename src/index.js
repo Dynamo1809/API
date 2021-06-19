40a62f93ac9aee9b3e1993623f9c10f7285ec1bc
Dmytro Maksiuta <d.maksiuta18@gmail.com>
2021-06-19 18:12:59 +0300

    api-change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 'use strict';
 import './sass/main';
-// import API from './fetchEvents.js';
 import eventsTpl from './templates/events.hbs';
 import countries from './json/countries.json';
 import selectOptionsTpl from './templates/selectOptions';
-// import countryTpl from './templates/country.hbs';
 // import { error } from '@pnotify/core';
 const debounce = require('lodash.debounce');
 const axios = require('axios');
@@ -20,78 +18,29 @@ const refs = {
   eventList: document.querySelector('.event-card-set'),
   select: document.querySelector('.select')
 }
-
 // console.log(refs.select)
-
 refs.searchInput.addEventListener('input', debounce(onInputSearch, 500));
 
 markupHomePage()
 
-// let sizePage =20;
-
-// const onResize = function(e) {
-//   const width = e.target.outerWidth;
-//   console.log("onResize ~ width", width)
-//   // height = e.target.outerHeight;
-//   if( width >= 768 && width < 1280) {
-//     sizePage = 21;
-//   }else{
-//     sizePage = 20;
-//   }
-//   fetchEvents()
-//    console.log('in onResize',sizePage)
-  
-// }
-// window.addEventListener("resize", debounce(onResize, 250));
-
-// function fetchEvents(countryCode = ''){ 
-//   //-- Тестовая штука --//
-//   let sizePage;
-//   if(document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
-//     sizePage = 21;
-//   }else{
-//     sizePage = 20;
-//   }
-//   // window.addEventListener("resize", onResize);
-//   // document.documentElement.clientWidth
-//   // console.log(document.documentElement.clientWidth)
-//     // const date = new Date()
-//     // console.log(" date", date.toDateString())
-//     console.log('sizePage in fetch', sizePage)
-//     return axios.get(`${BASE_DISCOVERY_URL}events.json?countryCode=${countryCode}&sort=date,name,asc&size=${sizePage}&apikey=${DISCOVERY_KEY}`)
-//       .then(response => {
-//         console.log(response.data._embedded)
-//         // console.log(response.data._embedded.forEach(r => console.log(r)))
-//         return response.data
-//       }) 
-//     // return axios.get(`${BASE_DISCOVERY_URL}events.json?keyword=${searchQuery}&sort=date,name,asc&apikey=${DISCOVERY_KEY}`)
-//     // .then(response => response.data);     
-// }  
-
-
 async function fetchEvents(countryCode = ''){ 
   let sizePage;
   let page = 0;
-  try{
     if (document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
     sizePage = 21;
     } else{
     sizePage = 20;
     };
-
     const data = await axios.get(`${BASE_DISCOVERY_URL}events.json?countryCode=${countryCode}
-    &sort=date,name,asc&size=${sizePage}&page=${page}&&apikey=${DISCOVERY_KEY}`)
+    &sort=date,name,asc&size=${sizePage}&page=${page}&apikey=${DISCOVERY_KEY}`)
       .then(response => {
         page += 1;
         // const embeddeds = response.data._embedded
         // console.log(Array.from((response.data._embedded), (v,k) => k))
-        return response.data
+        return response.data._embedded
       }) 
       // console.log('data', data._embedded )
       return data;
-  }catch (err) {
-    console.log(err)  ;
-  }
   // .forEach(r => console.log(r))
   // Object.values()
 }  
@@ -99,31 +48,51 @@ async function fetchEvents(countryCode = ''){
 // console.log(Array.from({events:[1,2,3,4,5,6,7,8,9,]}, (v,k) => k))
 
 async function fetchUserCountryCodeByIp() {
-  try{
     const userCountryCode = await axios.get(`${BASE_IPSTACK_URL}check?access_key=${IPSTACK_KEY}`)
       .then(response => response.data.country_code);
     return userCountryCode;
-  }catch (error){
-    console.log(error);
-  }
 }
       // const data = await fetchEvents(countryCode)
 async function markupHomePage() {
   try{
-    await fetchUserCountryCodeByIp()
-    .then(countryCode => {
-       refs.select.value = countryCode;
-       return fetchEvents(countryCode)
-    })
-    .then(response => {
-      if(!response._embedded){
-        console.log(response)
-        refs.select.value = '';
-        return fetchEvents()
-                .then(response => {
-                  return createEventMarkup(response._embedded)
-                })
-        // refs.select.value = countryCode;
+    const countryCode = await fetchUserCountryCodeByIp();
+    console.log('markupHomePage ~ countryCode', countryCode)
+    fetchEvents(countryCode).then(r => {
+      if(!r) {
+        fetchEvents('').then(r => createEventMarkup(r.events))
+      }else{
+        createEventMarkup(r.events)
+        refs.select.value = countryCode;
+      }
+    }) 
+    //   refs.select.value = '';
+    //   fetchEvents('').then(createEventMarkup )
+    // }else{
+    //   refs.select.value = countryCode;
+    //   fetchEvents(countryCode).then(r => createEventMarkup(r.events))
+    // }
+    // .catch(err =>console.log(err))
+    // .then(countryCode => {
+    //    refs.select.value = countryCode;
+    //    fetchEvents(countryCode).then( r => createEventMarkup(r))
+    //    return fetchEvents(countryCode)
+    // })
+    // .then(response => {
+    //   if(!response){
+    //     console.log(response)
+    //     refs.select.value = '';
+    //     return fetchEvents()
+    //             .then(response => {
+    //               return createEventMarkup(response._embedded)
+    //             })
+    //   }
+    // })
+    }catch (error){
+      console.log(error)
+    }
+}
+
+// refs.select.value = countryCode;
         // console.log(response._embedded.events)
         // createEventMarkup(response._embedded)
         // console.log(response._embedded.events.map(event => event))
@@ -131,9 +100,7 @@ async function markupHomePage() {
         // const currentTime = Date.now();
         // const filterByCurrentDate = await response._embedded.events.filter(event => currentTime < new.Date(event.dates.start.localDate) )
                   // console.log('markupHomePage ~ filterByCurrentDate', filterByCurrentDate)
-        // console.log('selectRef', refs.select.value)
-        console.log("NO")
-       
+        // console.log('selectRef', refs.select.value)     
       // } else{
       //   refs.select.value = '';
       //   // console.log('~ refs.select.value', refs.select.value)   
@@ -144,33 +111,18 @@ async function markupHomePage() {
       //             // console.log(response._embedded.events[1].dates.start.localDate)
       //             // const eventDate = response._embedded.events[1].dates.start.localDate;
       //             // console.log('events',response._embedded.events)
-      //             // console.log('markupHomePage ~ eventDate', eventDate)
-                  
+      //             // console.log('markupHomePage ~ eventDate', eventDate)                 
       //             // const currentTime = Date.now();
-
       //             // const filterByCurrentDate = response._embedded.events.filter(event => currentTime < new.Date(event.dates.start.localDate) )
       //             // console.log('markupHomePage ~ filterByCurrentDate', filterByCurrentDate)
-
       //           //  const testDate = new Date(eventDate)
       //           //  console.log(testDate.getTime())
-
-      //           //   const date = Date.now(eventDate)
-                  
-                  
+      //           //   const date = Date.now(eventDate)                                
       //             // const deltaTime = currentTime - testDate.getTime() ;
-      //             // console.log('markupHomePage ~ deltaTime', deltaTime)
-                  
-      //             // console.log('markupHomePage ~ date', date)
-                  
+      //             // console.log('markupHomePage ~ deltaTime', deltaTime)               
+      //             // console.log('markupHomePage ~ date', date)               
       //             return createEventMarkup(response._embedded)
       //           })
-      }
-    })
-    }catch (error){
-      console.log(error)
-    }
-}
-
 
 function onInputSearch(e) {
   resetPage();
@@ -233,3 +185,45 @@ refs.select.insertAdjacentHTML('beforeend',optionsMarkup);
   //     delay: 500,
   //   });
   // }
+
+
+  // let sizePage =20;
+
+// const onResize = function(e) {
+//   const width = e.target.outerWidth;
+//   console.log("onResize ~ width", width)
+//   // height = e.target.outerHeight;
+//   if( width >= 768 && width < 1280) {
+//     sizePage = 21;
+//   }else{
+//     sizePage = 20;
+//   }
+//   fetchEvents()
+//    console.log('in onResize',sizePage)
+  
+// }
+// window.addEventListener("resize", debounce(onResize, 250));
+
+// function fetchEvents(countryCode = ''){ 
+//   //-- Тестовая штука --//
+//   let sizePage;
+//   if(document.documentElement.clientWidth >= 768 && document.documentElement.clientWidth < 1280) {
+//     sizePage = 21;
+//   }else{
+//     sizePage = 20;
+//   }
+//   // window.addEventListener("resize", onResize);
+//   // document.documentElement.clientWidth
+//   // console.log(document.documentElement.clientWidth)
+//     // const date = new Date()
+//     // console.log(" date", date.toDateString())
+//     console.log('sizePage in fetch', sizePage)
+//     return axios.get(`${BASE_DISCOVERY_URL}events.json?countryCode=${countryCode}&sort=date,name,asc&size=${sizePage}&apikey=${DISCOVERY_KEY}`)
+//       .then(response => {
+//         console.log(response.data._embedded)
+//         // console.log(response.data._embedded.forEach(r => console.log(r)))
+//         return response.data
+//       }) 
+//     // return axios.get(`${BASE_DISCOVERY_URL}events.json?keyword=${searchQuery}&sort=date,name,asc&apikey=${DISCOVERY_KEY}`)
+//     // .then(response => response.data);     
+// }  
